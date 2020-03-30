f9c219ed390ad1c6157be388a05e8498d56b48cf
nickzaf95 <nicholaszafiriou@gmail.com>
2020-03-30 13:44:12 +0100

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const testVar = {}
 
-function testFunc() {
-  return "hi"
+function isWin(year) {
+  return year.result === "W"
+}
+
+function superbowlWin(object) {
+  if (object.find( isWin )) {
+    return object.find( isWin ).year
+  } else {
+    return undefined
+  }
 }
